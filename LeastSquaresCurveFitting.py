1f08a1119f85f670156e709caad7c2a05ebac43c
praveen <praveenm@umd.edu>
2020-02-07 23:23:43 -0500

    Adding Problem 2 current solution

---
+++ LeastSquaresCurveFitting.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 14:04:08 2020
+
+@author: Praveen
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def leastSquares_curveFitting():
+    curve_data = np.genfromtxt('data_2.csv', delimiter = ',')
+    x = curve_data[1:,0]
+    y = curve_data[1:,1]
+    x_squared = np.power(x,2)
+    x_matrix = np.transpose([x_squared, x, np.ones(np.shape(x))])
+    B = np.matmul(np.linalg.inv(np.matmul(np.transpose(x_matrix), x_matrix)),
+                  np.matmul(np.transpose(x_matrix), y))
+    y_estimate = np.matmul(x_matrix, np.transpose(B))
+    plt.scatter(x, y, label = 'Scattered data')
+    plt.plot(x, y_estimate, 'r', label = 'Curve Fitting')
+    plt.legend()
+    
+def main():
+    leastSquares_curveFitting()
+    
+if __name__=='__main__':
+    main()
+
+
