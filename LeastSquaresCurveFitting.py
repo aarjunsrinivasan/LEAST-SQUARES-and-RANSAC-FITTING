2904df48f4574c56273dbcf3b12c46291047bda3
praveen <praveenm@umd.edu>
2020-02-11 15:46:31 -0500

    Updated with comments and description

--- LeastSquaresCurveFitting.py
+++ LeastSquaresCurveFitting.py
@@ -9,15 +9,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def leastSquares_curveFitting():
+    '''
+    Function to fit a curve using Least squares given minimum number of points (x,y) from the data.
+    Y = XB
+    E = (Y - XB)T(Y - XB)
+    dE/dB = 0
+    B = inv(XTX)*XT*Y 
+    '''
+    # Load curve data from csv files
     curve_data = np.genfromtxt('data_2.csv', delimiter = ',')
+     # Extract x and y coordinates from the curve data
     x = curve_data[1:,0]
     y = curve_data[1:,1]
     x_squared = np.power(x,2)
     x_matrix = np.transpose([x_squared, x, np.ones(np.shape(x))])
     B = np.matmul(np.linalg.inv(np.matmul(np.transpose(x_matrix), x_matrix)),
                   np.matmul(np.transpose(x_matrix), y))
-    print(B)
+     # Estimate the y coordinate of the curve data for the evaluated model parameters
     y_estimate = np.matmul(x_matrix, np.transpose(B))
+    # Plot curve data, inlier data, curve estimate
     plt.scatter(x, y, label = 'Scattered data')
     plt.plot(x, y_estimate, 'r', label = 'Curve Fitting')
     plt.legend()
